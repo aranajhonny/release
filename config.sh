052bf8b76a30306ceece4d4794f5fc3094bd4718
Jhonny <aranajhonny@users.noreply.github.com>
2023-07-10 17:34:26 -0500

    .

---
+++ config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cat > ~/membrane/.config <<EOF
+{
+  "access_token": "$MCTL_TOKEN",
+  "refresh_token": "v1.MTBpZ_mMXZucCsjiAKWTs7lqJWDCIuClXuIWYFd_eKi6EFbUYoHZikeK9qaMgnNWMs2W3hFOXzzaxrTSJAhZmRs",
+  "scope": "offline_access",
+  "token_type": "Bearer",
+  "expires_at": 1691593067
+}
+EOF
