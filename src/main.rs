50699c31f4d60f53afdf5b157f11fe300cac3685
Jhonny <aranajhonny@users.noreply.github.com>
2023-07-13 01:12:35 -0500

    add recursion

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use git2::{Error, Repository};
-use reqwest::{Client, Error as HttpError, StatusCode};
+use anyhow::anyhow;
+use async_recursion::async_recursion;
+use git2::Repository;
+use reqwest::Client;
 use serde::Serialize;
-use serde_json::json;
+use serde_json::{json, Value};
+use std::collections::HashSet;
 use std::io::Write;
 use std::{
     env, fs,
@@ -11,6 +14,13 @@ use std::{
     process::{exit, Command},
 };
 
+#[derive(Debug)]
+struct Program {
+    name: String,
+    dependencies: Vec<String>,
+    npm_dependencies: bool,
+}
+
 #[derive(Serialize)]
 struct TestResult {
     program: String,
@@ -18,15 +28,15 @@ struct TestResult {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    setup_http()?;
-
+async fn main() -> Result<(), anyhow::Error> {
+    // Clone a Membrane directory
     let url = "https://github.com/membrane-io/directory.git";
+
     let repo = match Repository::clone(url, "directory") {
         Ok(repo) => repo,
         Err(e) => panic!("failed to clone: {}", e),
     };
-
+    // Update all submodules
     for mut sub in repo.submodules()? {
         sub.update(true, None)?;
         println!("{}", sub.name().unwrap());
@@ -40,120 +50,137 @@ async fn main() -> Result<(), Error> {
         .expect("Failed to get current directory")
         .join("directory");
 
+    // Copy the directory to the home directory
     let _ = copy_folder(&source_folder, &membrane_dir);
-
     println!("Folder copied successfully!");
 
-    let entries: Vec<_> = match fs::read_dir(&membrane_dir) {
+    // create a array of all the folders in the directory
+    // skip the .git folder
+    let mut entries: Vec<_> = match fs::read_dir(&membrane_dir) {
         Ok(entries) => entries,
         Err(err) => {
             eprintln!("Error reading directory: {}", err);
-            exit(1);
+            std::process::exit(1);
         }
     }
+    .filter_map(Result::ok)
+    .filter(|entry| {
+        let path = entry.path();
+        entry.file_type().map(|ft| ft.is_dir()).unwrap_or(false)
+            && path.file_name().map(|name| name != ".git").unwrap_or(false)
+    })
+    .map(|entry| entry.file_name().to_string_lossy().into_owned())
     .collect();
 
-    for entry in &entries[..] {
-        if let Ok(entry) = entry {
-            // Check if the entry is a directory
-            if let Ok(file_type) = entry.file_type() {
-                if file_type.is_dir() {
-                    // Extract the subfolder name
-                    let program = match entry.file_name().into_string() {
-                        Ok(name) => name,
-                        Err(_) => {
-                            eprintln!("Error extracting folder name");
-                            continue;
-                        }
-                    };
-
-                    let package_json_path = entry.path().join("package.json");
-                    if package_json_path.exists() {
-                        // Run the `yarn` command in the subfolder
-                        let command = Command::new("yarn").current_dir(entry.path()).spawn();
-                        println!("Running yarn in {}", program);
-                        match command {
-                            Ok(mut child) => {
-                                if let Err(err) = child.wait() {
-                                    eprintln!("Error executing command: {}", err);
-                                }
-                            }
-                            Err(err) => {
-                                eprintln!("Error spawning command: {}", err);
-                            }
-                        }
-                    }
-                    mctl_update(&program);
-                }
-            }
-        }
-    }
-    let mut all_results = Vec::new();
-
-    // Separate iteration for running tests
-    for entry in &entries[..] {
-        if let Ok(entry) = entry {
-            if let Ok(file_type) = entry.file_type() {
-                if file_type.is_dir() {
-                    let program = entry.file_name();
-                    let result = mctl_test(program.to_str().unwrap());
-                    if !result.success {
-                        let message = format!("❌ Test failed for {:?}", program);
-                        if let Err(err) = send_message(&message).await {
-                            let err_message = format!("HTTP Error: {}", err);
-                            return Err(Error::from_str(&err_message));
-                        }
-                    }else {
-                        let message = format!("🎉 Test passed for {:?}", program);
-                        if let Err(err) = send_message(&message).await {
-                            let err_message = format!("HTTP Error: {}", err);
-                            return Err(Error::from_str(&err_message));
-                        }
-                    }
-                    all_results.push(result);
-                }
-            }
-        }
-    }
-
-    // Save all test results to JSON
-    if let Ok(json_data) = serde_json::to_string_pretty(&all_results) {
-        let file_path = "all_results.json";
-        if let Ok(mut file) = File::create(file_path) {
-            if let Err(err) = file.write_all(json_data.as_bytes()) {
-                eprintln!("Error writing test results to file: {}", err);
-            }
+    // Order the entries so that http is first
+    entries.sort_by(|a, b| {
+        if a == "http" {
+            std::cmp::Ordering::Less
+        } else if b == "http" {
+            std::cmp::Ordering::Greater
         } else {
-            eprintln!("Error creating file for test results");
+            a.cmp(b)
         }
-    } else {
-        eprintln!("Error serializing test results to JSON");
+    });
+
+    let mut installed_programs: HashSet<String> = HashSet::new();
+    let mut programs: Vec<Program> = Vec::new();
+
+    // Get all the dependencies for each program
+    for entry in entries {
+        if let Ok(program) = get_dependencies(&entry) {
+            programs.push(program);
+        }
     }
+
+    // Check all the dependencies and install them recursively
+    for program in &programs {
+        check_dependencies(&programs, program, &mut installed_programs).await;
+    }
+
+    // let mut all_results: Vec<TestResult> = Vec::new();
+    // // Save all test results to JSON
+    // if let Ok(json_data) = serde_json::to_string_pretty(&all_results) {
+    //     let file_path = "all_results.json";
+    //     if let Ok(mut file) = File::create(file_path) {
+    //         if let Err(err) = file.write_all(json_data.as_bytes()) {
+    //             eprintln!("Error writing test results to file: {}", err);
+    //         }
+    //     } else {
+    //         eprintln!("Error creating file for test results");
+    //     }
+    // } else {
+    //     eprintln!("Error serializing test results to JSON");
+    // }
 
     Ok(())
 }
 
-fn setup_http() -> Result<(), Error> {
-    let url = "https://github.com/juancampa/membrane-http-program.git";
+// execute a update command for a program in the membrane directory
+// if has a package.json file, use yarn
+// then ren the mctl test command for the program
+async fn run_program(program: &Program) -> Result<(), anyhow::Error> {
+    if program.npm_dependencies {
+        yarn_install(&program.name);
+    }
+    mctl_update(&program.name);
+    let result = mctl_test(&program.name);
+    if !result.success {
+        let message = format!("❌ Test failed for {:?}", program);
+        if let Err(err) = send_message(&message).await {
+            let err_message = format!("HTTP Error: {}", err);
+            return Err(anyhow!(err_message));
+        }
+    } else {
+        let message = format!("🎉 Test passed for {:?}", program);
+        if let Err(err) = send_message(&message).await {
+            let err_message = format!("HTTP Error: {}", err);
+            return Err(anyhow!(err_message));
+        }
+    }
+    Ok(())
+}
 
-    let membrane_dir = dirs::home_dir()
+#[async_recursion]
+async fn check_dependencies(
+    programs: &[Program],
+    program: &Program,
+    installed_programs: &mut HashSet<String>,
+) {
+    if !program.dependencies.is_empty() {
+        for dependency_name in &program.dependencies {
+            let dependency = programs.iter().find(|p| p.name == *dependency_name);
+            if let Some(dependency) = dependency {
+                check_dependencies(programs, dependency, installed_programs).await;
+            } else {
+                println!("Dependency not found: {}", dependency_name);
+            }
+        }
+    }
+
+    if installed_programs.insert(program.name.clone()) {
+        let _ = run_program(&program).await;
+    }
+}
+
+fn yarn_install(program: &str) {
+    let path = dirs::home_dir()
         .expect("Failed to get home directory")
         .join("membrane")
-        .join("http");
+        .join(program);
 
-    match Repository::clone(url, "http") {
-        Ok(repo) => repo,
-        Err(e) => panic!("failed to clone: {}", e),
-    };
+    let command = Command::new("yarn").current_dir(&path).spawn();
 
-    let source_folder = env::current_dir()
-        .expect("Failed to get current directory")
-        .join("http");
-
-    let _ = copy_folder(&source_folder, &membrane_dir);
-
-    mctl_update("http");
-    Ok(())
+    match command {
+        Ok(mut child) => {
+            if let Err(err) = child.wait() {
+                eprintln!("Error executing command: {}", err);
+            }
+        }
+        Err(err) => {
+            eprintln!("Error spawning command: {}", err);
+        }
+    }
 }
 
 fn mctl_update(program: &str) {
@@ -228,10 +255,11 @@ fn copy_folder(source: &Path, destination: &Path) -> io::Result<()> {
 
     Ok(())
 }
-async fn send_message(message: &str) -> Result<(), HttpError> {
+
+async fn send_message(message: &str) -> Result<(), anyhow::Error> {
     let client = Client::new();
     // TODO: replace with webhook URL
-    let url = "https://discord.com/api/webhooks/1128740471552880640/tExzZQ1LmRDWnP_qrTDVgBIzwZ5ewpMmQYL8FcU6OY6tuB74HMe5BV5mXVAJ7oEIMdMY";
+    let url = discord_url();
 
     let params = json!({
         "username": "Test Bot",
@@ -248,8 +276,62 @@ async fn send_message(message: &str) -> Result<(), HttpError> {
     let status_code = response.status().as_u16();
     if status_code > 299 {
         println!("Error sending message: {:?}", response);
-    } else {
-        println!("Message sent successfully");
     }
     Ok(())
+}
+
+fn discord_url() -> String {
+    match std::env::var("DISCORD_WEBHOOK_URL") {
+        Ok(key) => key,
+        Err(_r) => {
+            eprintln!("DISCORD_WEBHOOK_URL not set");
+            "".to_string()
+        }
+    }
+}
+
+fn get_dependencies(program_name: &str) -> Result<Program, std::io::Error> {
+    let membrane_dir = dirs::home_dir()
+        .expect("Failed to get home directory")
+        .join("membrane");
+    let folder_path = Path::new(&membrane_dir).join(program_name);
+    let config_path = folder_path.join("memconfig.json");
+    let package_json_path = folder_path.join("package.json");
+
+    if let Ok(contents) = fs::read_to_string(&config_path) {
+        let dependencies = extract_program_names(&contents);
+        let program = Program {
+            name: String::from(program_name),
+            dependencies,
+            npm_dependencies: package_json_path.exists(), // Check if package.json exists
+        };
+        Ok(program)
+    } else {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            "Failed to read config file",
+        ))
+    }
+}
+
+fn extract_program_names(json_str: &str) -> Vec<String> {
+    let json_value: Value = serde_json::from_str(json_str).unwrap();
+    let dependencies = json_value["dependencies"].as_object().unwrap();
+
+    let mut program_names: Vec<String> = Vec::new();
+
+    for (_, value) in dependencies.iter() {
+        if let Some(colon_idx) = value.as_str().and_then(|s| s.find(':')) {
+            let program_name = value.as_str().unwrap()[..colon_idx].to_owned();
+
+            if !program_name.starts_with("sys-") {
+                program_names.push(program_name);
+            }
+        }
+    }
+
+    program_names.sort();
+    program_names.dedup();
+
+    program_names
 }
